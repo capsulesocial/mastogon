9d3a3c289a5b2cec737819c5a378e2b774c29ab7
Nadim Kobeissi <nadim@symbolic.software>
2022-11-08 16:10:23 +0100

    WIP

--- cmd/mastogon/main.go
+++ cmd/mastogon/main.go
@@ -4,38 +4,30 @@
 package cmd
 
 import (
-	"os"
+	"log"
+	"mastogon/internal/db"
+	"mastogon/internal/service"
+	"sync"
 
+	"github.com/go-fed/activity/pub"
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "mastogon",
 	Short: "Mastodon but in Go, basically. ActivityPub! Fediverse!",
 	Long:  `Long description`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		s := &service.Service{}
+		db := db.DB{}
+		db.Construct(&sync.Map{}, &sync.Map{}, "localhost")
+		actor := pub.NewFederatingActor(s, s, db, s)
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func main() {
 	err := rootCmd.Execute()
 	if err != nil {
-		os.Exit(1)
+		log.Fatal(err)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mastogon.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.19
 
 require (
 	github.com/go-fed/activity v1.0.0 // indirect
+	github.com/go-fed/httpsig v0.1.1-0.20190914113940-c2de3672e5b5 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20180527072434-ab813273cd59 // indirect
+	golang.org/x/sys v0.0.0-20180525142821-c11f84a56e43 // indirect
 )
